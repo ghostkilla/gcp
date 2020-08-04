0e29e4990329f90dc687cd5a1d191ab982cf48ce
ghostkilla <sando8307@gmail.com>
2020-08-04 17:04:15 +0900

    update

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # gcp
 
-### API有効化一覧
+## 概要
+意味ないけどfirestoreに保存してすぐ取り出すだけのbackendアプリケーション。  
+動作確認ができればおk
+
+### 使用GCPリソース
 - Cloud Build
+- Cloud Functions
+- Firestore(FirebaseのFirestoreではない)
+
+### 必須API有効化一覧
+- Cloud Build
+- Cloud Functions
 - Cloud Resource Manager
+
+### Cloud Build サービス アカウント権限設定（Cloud Buildの設定メニュー）
+Cloud FunctionsとService Accountsを有効にする。
+
+### やったこと（学んだこと）
+- Cloud Buildを利用したGithubと連携したCD/CI環境構築
+- Cloud Buildを利用したCloud Functionsデプロイ
+- TypescriptのソースコードをCloud Functionsへデプロイ
+- Firestore利用
+- IAMの設定（権限周り）
+- Google Cloud API周り
+- Cloud FunctionsとFirestoreのローカル環境構築（docker）
--- app/index.ts
+++ app/index.ts
@@ -2,13 +2,37 @@
 
 import express from 'express'
 import cors from 'cors'
+import { Firestore } from '@google-cloud/firestore'
+
+let firestore: Firestore
+
+if (process.env.ENV === 'local') {
+  const grpc = require('@grpc/grpc-js')
+  firestore = new Firestore({
+    projectId: 'firestore-emulator-sample',
+    servicePath: 'firestore',
+    port: 8974,
+    sslCreds: grpc.credentials.createInsecure(),
+    customHeaders: {
+      "Authorization": "Bearer owner"
+    }
+  })
+} else {
+  firestore = new Firestore()
+}
 
 const app = express()
 app.use(cors())
 app.options('*', cors())
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.status(200).send('Hello World!')
+app.get('/', async (req: express.Request, res: express.Response) => {
+  const docPath = 'test/1'
+  const messages = ['大吉', '吉', '中吉', '小吉', '末吉', '凶']
+  await firestore.doc(docPath).set({
+    message: messages[Math.floor(Math.random() * messages.length)]
+  })
+  const message = (await firestore.doc(docPath).get()).get('message')
+  res.status(200).send(`[GET] 今日も一日 !!! ${message} !!!`)
 })
 
 exports.handler = app
